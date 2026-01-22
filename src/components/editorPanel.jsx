3f09a852d6a5923c6109811cef51986309ccb70b
anuragmaganti <amaganti.dev@gmail.com>
2026-01-22 16:16:45 -0500

    set up base layout and component structure

---
+++ src/components/editorPanel.jsx
@@ -0,0 +1,13 @@
+import SectionTabs from "./sectionTabs"
+
+export default function EditorPanel() {
+    return (
+        <>
+            <div>Editor Panel</div>
+            <SectionTabs></SectionTabs>
+        
+        </>
+        
+
+    )
+}
---
+++ src/components/sectionTabs.jsx
@@ -0,0 +1,5 @@
+export default function SectionTabs() {
+    return(
+        <>SECTION TABS.........</>
+    )
+}
