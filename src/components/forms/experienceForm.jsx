3dd7dee1b90f64149da56ee12cd83b69ba4f01db
anuragmaganti <amaganti.dev@gmail.com>
2026-01-26 15:51:41 -0500

    removed local state and passed down global resume state to experience section

--- src/components/forms/experienceForm.jsx
+++ src/components/forms/experienceForm.jsx
@@ -1,36 +1,106 @@
-import { useState } from "react";
+export default function ExperienceForm({experience,setResume}) {
 
-export default function ExperienceForm() {
-
-    const [experienceEntries,setExperienceEntries] = useState([
-        {id: crypto.randomUUID(), company:"",activites:"",yearsExp:""}
-    ])
 
     function handleSubmit(e) {
         e.preventDefault();
     }
 
+    // return (
+    //     <div>
+    //         {experienceEntries.map((entry)=> (
+    //         <fieldset key={entry.id}>
+    //             <form onSubmit={handleSubmit}>
+    //                 <label htmlFor={"company"+entry.id}>Company: </label>
+    //                 <input type="text" id={"company"+entry.id} name="company"/>
+    //                 <div></div>
+    //                 <label htmlFor={"activites"+entry.id}>Description of activites: </label>
+    //                 <textarea id={"activites"+entry.id} name="activites"></textarea>
+    //                 <div></div>
+    //                 <label htmlFor={"yearsExp"+entry.id}>Years active: </label>
+    //                 <input type="text" id={"yearsExp"+entry.id} name="yearsExp"/>
+    //                 <div></div>
+    //                 <button type="button" onClick={()=>setExperienceEntries(experienceEntries.filter(item => item.id !== entry.id))}>Delete</button>
+    //             </form>
+    //             <div>Entry #{entry.id}</div>
+    //         </fieldset> ))}
+    //         <button type="button" onClick={()=>setExperienceEntries([...experienceEntries,{id: crypto.randomUUID(),company:"",activites:"",yearsExp:""}])}>Add Another</button>
+    //         <div></div>
+    //         <button type="button">Update Resume</button>
+    //     </div>
+    // )
+
     return (
         <div>
-            {experienceEntries.map((entry)=> (
+            {experience.map((entry)=>(
             <fieldset key={entry.id}>
                 <form onSubmit={handleSubmit}>
                     <label htmlFor={"company"+entry.id}>Company: </label>
-                    <input type="text" id={"company"+entry.id} name="company"/>
+                    <input type="text" id={"company"+entry.id} name="company" value={entry.company} onChange={
+                        (e)=>setResume(
+                            prev => (
+                                {...prev,
+                                experience: prev.experience.map( item =>
+                                    item.id === entry.id ? {...item, company: e.target.value} : item
+                                )
+                                }
+                            )
+                            
+                        )
+                        }/>
                     <div></div>
-                    <label htmlFor={"activites"+entry.id}>Description of activites: </label>
-                    <textarea id={"activites"+entry.id} name="activites"></textarea>
+                    <label htmlFor={"activities"+entry.id}>Activities: </label>
+                    <input type="text" id={"activities"+entry.id} name="activities" value={entry.activities} onChange={
+                        (e)=>setResume(
+                            prev => (
+                                {...prev,
+                                experience: prev.experience.map( item =>
+                                    item.id === entry.id ? {...item, activities: e.target.value} : item
+                                )
+                                }
+                            )
+                            
+                        )
+                        }/>
                     <div></div>
                     <label htmlFor={"yearsExp"+entry.id}>Years active: </label>
-                    <input type="text" id={"yearsExp"+entry.id} name="yearsExp"/>
+                    <input type="text" id={"yearsExp"+entry.id} name="yearsExp"value={entry.yearsExp} onChange={
+                        (e)=>setResume(
+                            prev => (
+                                {...prev,
+                                experience: prev.experience.map( item =>
+                                    item.id === entry.id ? {...item, yearsExp: e.target.value} : item
+                                )
+                                }
+                            )
+                            
+                        )
+                        }/>
                     <div></div>
-                    <button type="button" onClick={()=>setExperienceEntries(experienceEntries.filter(item => item.id !== entry.id))}>Delete</button>
+                    <button type="button" onClick={()=> setResume(
+                        prev => (
+                            {...prev,
+                                experience: prev.experience.filter(
+                                    item => 
+                                        item.id !== entry.id
+                                )
+                            }
+                        )
+                    )}>Delete</button>
+
                 </form>
                 <div>Entry #{entry.id}</div>
-            </fieldset> ))}
-            <button type="button" onClick={()=>setExperienceEntries([...experienceEntries,{id: crypto.randomUUID(),company:"",activites:"",yearsExp:""}])}>Add Another</button>
+            </fieldset>))}
+           
+            <button type="button" onClick={()=>setResume(
+                prev=> (
+                    {...prev,
+                        experience: [...prev.experience,{id: crypto.randomUUID(),company:"",activities:"",yearsExp:""}]
+                    }
+                )
+            )}>Add Another</button>
             <div></div>
-            <button type="button">Update Resume</button>
+            <button type="button">Update</button>
+
         </div>
     )
 }
