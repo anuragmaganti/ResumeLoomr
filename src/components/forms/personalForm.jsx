4e1c03f5e6f4bd60eacd2a4c10a60fad76085570
anuragmaganti <amaganti.dev@gmail.com>
2026-01-25 21:37:51 -0500

    connected personal form inputs to global resume state

--- src/components/forms/personalForm.jsx
+++ src/components/forms/personalForm.jsx
@@ -1,4 +1,4 @@
-export default function PersonalForm() {
+export default function PersonalForm({personal,setResume}) {
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -9,13 +9,24 @@ export default function PersonalForm() {
             <fieldset>
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="name">First and Last Name: </label>
-                    <input type="text" id="name" name="name"/>
+                    <input type="text" id="name" name="name" value={personal.name} onChange={(e)=> setResume(prev => (
+                        {...prev, personal: {...prev.personal, name:e.target.value}}
+                    )
+                    )}/>
                     <div></div>
                     <label htmlFor="phone">Phone Number: </label>
-                    <input type="text" id="phone" name="phone"/>
+                    <input type="text" id="phone" name="phone" value={personal.phone} onChange={(e)=> setResume(prev => (
+                        {...prev, personal: {...prev.personal, phone: e.target.value}}
+                    )
+                    )}/>
+
+
                     <div></div>
                     <label htmlFor="email">Email Address: </label>
-                    <input type="text" id="email" name="email"/>
+                    <input type="text" id="email" name="email" value={personal.email} onChange={(e) => setResume(prev => (
+                        {...prev,personal: {...prev.personal, email: e.target.value}}
+                    )
+                    )}/>
                     <div></div>
                     <button type="submit">Update</button>
                 </form>
