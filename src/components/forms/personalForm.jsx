bd0d8126881aac21e92ac27be598a5a94e701c24
anuragmaganti <amaganti.dev@gmail.com>
2026-01-25 21:58:36 -0500

    added helper function updatePersonal to update personal info in global state

--- src/components/forms/personalForm.jsx
+++ src/components/forms/personalForm.jsx
@@ -4,34 +4,33 @@ export default function PersonalForm({personal,setResume}) {
         e.preventDefault();
     }
 
+    function updatePersonal(e){
+        const personalInput=e.target.name;
+        const newValue=e.target.value;
+
+        setResume(prev => (
+            {...prev, personal: {...prev.personal, [personalInput]:newValue}}
+            )
+        )
+    }
+
+
     return (
         <div>
             <fieldset>
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="name">First and Last Name: </label>
-                    <input type="text" id="name" name="name" value={personal.name} onChange={(e)=> setResume(prev => (
-                        {...prev, personal: {...prev.personal, name:e.target.value}}
-                    )
-                    )}/>
+                    <input type="text" id="name" name="name" value={personal.name} onChange={(e)=> updatePersonal(e)}/>
                     <div></div>
                     <label htmlFor="phone">Phone Number: </label>
-                    <input type="text" id="phone" name="phone" value={personal.phone} onChange={(e)=> setResume(prev => (
-                        {...prev, personal: {...prev.personal, phone: e.target.value}}
-                    )
-                    )}/>
-
-
+                    <input type="text" id="phone" name="phone" value={personal.phone} onChange={(e)=>updatePersonal(e)}/>
                     <div></div>
                     <label htmlFor="email">Email Address: </label>
-                    <input type="text" id="email" name="email" value={personal.email} onChange={(e) => setResume(prev => (
-                        {...prev,personal: {...prev.personal, email: e.target.value}}
-                    )
-                    )}/>
+                    <input type="text" id="email" name="email" value={personal.email} onChange={(e)=>updatePersonal(e)}/>
                     <div></div>
                     <button type="submit">Update</button>
                 </form>
             </fieldset>
         </div>
-        
     )
 }
