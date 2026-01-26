2d6c222401f8c583e55549e440de60caff8e8eed
anuragmaganti <amaganti.dev@gmail.com>
2026-01-26 15:59:50 -0500

    render education and experience data to resume preview component

--- src/components/resumePreview.jsx
+++ src/components/resumePreview.jsx
@@ -1,9 +1,39 @@
 export default function ResumePreview({resume}) {
+    
+
     return (
         <div>
-            <h1>{resume.personal.name}</h1>
-            <h2>{resume.personal.phone}</h2>
-            <h2>{resume.personal.email}</h2>
+            <div className="personalSection">
+                <h1>{resume.personal.name}</h1>
+                <h3>{resume.personal.phone}</h3>
+                <h3>{resume.personal.email}</h3>
+            </div>
+
+            <div></div>
+
+            <div>
+                <h2>Education</h2>
+                {resume.education.map((item) => (
+                    <div>
+                        <div>{item.school}</div>
+                        <div>{item.yearsEdu}</div>
+                        <div>{item.degree}</div>
+                    </div>
+                ))}
+            </div>
+
+            <div></div>
+            
+            <div>
+                <h2>Experience</h2>
+                {resume.experience.map((item) => (
+                    <div>
+                        <div>{item.company}</div>
+                        <div>{item.yearsExp}</div>
+                        <div>{item.activities}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
