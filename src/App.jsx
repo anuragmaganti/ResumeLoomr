cf2e8aa7f1b41f2b73b625c64064e863994fd4c3
anuragmaganti <amaganti.dev@gmail.com>
2026-01-25 21:23:08 -0500

    passed global resume state and setter to EditorPanel

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     <div className="app">
       <Header></Header>
       <div className="main">
-        <EditorPanel activeTab={activeTab} setActiveTab={setActiveTab}></EditorPanel>
+        <EditorPanel activeTab={activeTab} setActiveTab={setActiveTab} resume={resume} setResume={setResume}></EditorPanel>
         <ResumePreview></ResumePreview>
       </div>
     </div>
