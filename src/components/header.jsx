0c7e5cb568b0ad26ac2ade5312a2c7ae094dc136
anuragmaganti <amaganti.dev@gmail.com>
2026-02-02 14:22:17 -0500

    changed header text to match domain name and added marginal padding to bottom

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header className="header">
-            <h1>Resume Builder</h1>
+            <h1>ResumeLoomr</h1>
         </header>
     )
 }
